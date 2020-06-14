010551e718e59dc9314b67dadc908e0fec98154c
nylachoirotun <31815400+nylachoirotun@users.noreply.github.com>
2020-06-14 13:18:14 +0700

    Update laporan_evaluasi_tanah_belum_shgb_per.php

--- application/views/member/laporan/laporan_evaluasi_tanah_belum_shgb_per.php
+++ application/views/member/laporan/laporan_evaluasi_tanah_belum_shgb_per.php
@@ -144,7 +144,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <section class="panel">
             <header class="panel-heading">    
                 <div class="row show-grid">
-                    <div class="col-md-6" align="left"><h2 class="panel-title">SERAH TERIMA KE TECHNIC</h2></div>
+                    <div class="col-md-6" align="left"><h2 class="panel-title">PROSES SHGB</h2></div>
                     <?php  
                     echo level_user('master','items',$this->session->userdata('kategori'),'add') > 0 ? '<div class="col-md-6" align="right"><a class="btn btn-success" href="#"  data-toggle="modal" data-target="#tambahData"><i class="fa fa-plus"></i> Tambah</a></div>':'';
                     ?> 
